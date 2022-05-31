b6a4141f78e485e5061f1a3d5f078fead46a3a2b
Karin van der Reijden <kjova@aqua.dtu.dk>
2022-05-31 11:11:49 +0200

    add Rscript

---
+++ DataCall_Rscript.R
@@ -0,0 +1,312 @@
+#####################################################################################################################
+#####################################################################################################################
+#
+#     Script to derive to requested tables from data call 
+#       "Data call concerning fisheries in Marine Protected Areas in the Baltic 
+#        and North Seas, Atlantic EU Western Waters and EU Outermost Regions"
+#
+#     By Karin van der Reijden (kjova@aqua.dtu.dk)
+#     May 2022
+#
+#####################################################################################################################
+#####################################################################################################################
+rm(list = ls())
+
+#-----------------------------------------------
+# Information on this script
+#-----------------------------------------------
+### Before running this code the following needs to be done:
+##   - Cleaned tacsatEflalo files for all the years should be in the RdataPath. Each file 
+##     should be saved as tacsatEflalo_"Year".rds (eg. tacsatEflalo_2019.rds)
+##     These files  should be in the standard tacsatEflalo format and contain merged kg and EURO for all
+##     species. This can be obtained using the countries own procedures or using 
+##     the 0_CreateTacsatEflalo.R script. 
+##   - Furthermore the cleaned tacsatEflalo should contain these columns:
+##     "VE_REF", "SI_LATI", "SI_LONG", "SI_DATE", "SI_TIME", "SI_SP", "SI_HE", "SI_STATE", "SI_DATIM", "INTV", "FT_REF"
+##   - Cleaned eflalo files should be in the RdataPath, saved as cleanEflalo_"Year".rds (eg. cleanEflalo_2019.rds)
+##   - The polygon shapefiles should be downloaded and extracted into the folder directed to by "polyPath"
+
+################################################
+# General settings
+################################################
+#-----------------------------------------------
+# Load libraries
+#-----------------------------------------------
+library(sf)
+library(data.table)
+library(vmstools)
+
+#-----------------------------------------------
+# Set data paths
+#-----------------------------------------------
+sysPath                       <- "Please provide here the link to your VMS data folder"
+RdataPath                     <- paste0(sysPath, "VMS/")
+polyPath                      <- paste0(sysPath, "Data/")
+intPath                       <- paste0(sysPath, "Int/")
+resPath                       <- paste0(sysPath, "Results/")
+
+# If directories do not exist, create them
+dir.create(sysPath,    showWarnings = FALSE)
+dir.create(polyPath,   showWarnings = FALSE)
+dir.create(intPath,    showWarnings = FALSE)
+dir.create(resPath,    showWarnings = FALSE)
+dir.create(RdataPath,  showWarnings = FALSE)
+
+#-----------------------------------------------
+# Load MPA polygon data and merge to 1 file
+#-----------------------------------------------
+MPAs1                         <- st_read(paste0(polyPath, "MPA_polygons1.shp"))
+MPAs2                         <- st_read(paste0(polyPath, "MPA_polygons2.shp"))
+MPAs                          <- rbind(MPAs1, MPAs2)
+rm(MPAs1, MPAs2)
+
+#-----------------------------------------------
+# Set some variables
+#-----------------------------------------------
+Country                       <- "DK"
+yearsToSubmit                 <- c(2012:2021)
+
+#-----------------------------------------------
+
+
+
+################################################
+# Processing of tacset and eflalo files
+################################################
+#-----------------------------------------------
+# Read in the data, per year, and process
+#-----------------------------------------------
+for(iYear in yearsToSubmit) {
+  print(paste(Sys.time(), iYear))
+  
+  tacsatEflalo                <- readRDS(paste0(RdataPath,"tacsatEflalo_",iYear,".rds"))
+  eflalo                      <- readRDS(paste0(RdataPath,"cleanEflalo_",iYear,".rds"))
+  
+  # ## If data is saved as .RData, use this instead
+  # load(file=paste0(RdataPath,"tacsatEflalo_",Year,".RData"))
+  # load(file=paste0(RdataPath,"cleanEflalo_",Year,".RData"))
+  
+  #-----------------------------------------------
+  # Prepare tacsatEflalo and eflalo
+  #-----------------------------------------------
+  # Add relevant information to tacsatEflalo
+  tacsatEflalo$LE_RECT        <- ICESrectangle(tacsatEflalo)
+  tacsatEflalo$LE_GEAR        <- eflalo$LE_GEAR[match(tacsatEflalo$FT_REF, eflalo$FT_REF)]
+  tacsatEflalo$LE_QUART       <- as.integer(quarter(tacsatEflalo$SI_DATIM))
+  tacsatEflalo$VE_KW          <- eflalo$VE_KW[match(tacsatEflalo$FT_REF, eflalo$FT_REF)]
+  tacsatEflalo$LE_MET         <- eflalo$LE_MET[match(tacsatEflalo$FT_REF, eflalo$FT_REF)] # This should be the level 6 metier definitions. Please adjust the script if needed.
+  eflalo$LE_QUART             <- quarter(as.Date(eflalo$LE_CDAT, format="%d/%m/%Y"))
+  eflalo$LE_YEAR              <- iYear
+  tacsatEflalo$LE_YEAR        <- iYear
+  
+  # Make data.frames into data.tables for faster processing
+  te                          <- data.table(tacsatEflalo); gc()
+  e                           <- data.table(eflalo)
+  
+  #-----------------------------------------------
+  # Calculate total landings (kg / EURO)
+  #-----------------------------------------------
+  # Calculate total kg and EURO if not already present in data as LE_KG_TOT and LE_EURO_TOT
+  if(!"LE_KG_TOT" %in% colnames(te))
+    te[,LE_KG_TOT:=rowSums(.SD, na.rm = TRUE), .SDcols = grep("KG", names(te))]
+  if(!"LE_EURO_TOT" %in% colnames(te))
+    te[,LE_EURO_TOT:=rowSums(.SD, na.rm = TRUE), .SDcols = grep("EURO", names(te))]
+  if(!"LE_KG_TOT" %in% colnames(e))
+    e[,LE_KG_TOT:=rowSums(.SD, na.rm = TRUE), .SDcols = grep("KG", names(e))]
+  if(!"LE_EURO_TOT" %in% colnames(e))
+    e[,LE_EURO_TOT:=rowSums(.SD, na.rm = TRUE), .SDcols = grep("EURO", names(e))]
+  
+  # Calculate total fishing effort in days and kwdays 
+  te[, LE_EFF_DAY := INTV/1440]
+  te[, LE_EFF_KWD := (LE_EFF_DAY) * VE_KW]
+  
+  #-----------------------------------------------
+  # Save te and e for later usage
+  #-----------------------------------------------
+  saveRDS(te, paste0(intPath, "TACEFL_", iYear, ".rds"))
+  saveRDS(e, paste0(intPath, "EFLA_", iYear, ".rds"))
+} # end iYear-loop
+
+#-----------------------------------------------
+
+
+
+################################################
+# Create Table 1
+################################################
+#-----------------------------------------------
+# Obtain data and process
+#-----------------------------------------------
+m_list                       <- list.files(path = intPath, pattern= "TACEFL", full.names = T)
+dat                          <- rbindlist(lapply(m_list, readRDS), fill=T)
+# Set NA's to 0 
+dat[is.na(dat)]              <-  0
+
+# Assign Csquare based on lon and lat
+dat$Csquare                  <- CSquare(lon = dat$SI_LONG,
+                                        lat = dat$SI_LATI,
+                                        degrees = 0.05)
+
+#-----------------------------------------------
+# Create table 1
+#-----------------------------------------------
+# Aggregate data and write to results folder
+table1                       <- dat[, .(Land_kg = sum(LE_KG_TOT),
+                                        Eff_day = sum(LE_EFF_DAY, na.rm=T),
+                                        Eff_kwh = sum(LE_EFF_KWD, na.rm=T)),
+                                    by=.(LE_YEAR, Csquare, LE_MET)]
+
+# Save as csv-file
+fwrite(table1, paste0(resPath, "Table_1_", Country, ".csv"))
+rm(m_list, dat, table1)
+#-----------------------------------------------
+
+
+
+################################################
+# Create Tables 2, 3a and 3b
+################################################
+#-----------------------------------------------
+# Create yearly MPA-aggregates
+#-----------------------------------------------
+for(iYear in yearsToSubmit) {
+  print(paste(Sys.time(), iYear))
+  
+  te                         <- readRDS(file=paste0(intPath, "TACEFL_", iYear, ".rds"))
+  #-----------------------------------------------
+  # Convert to spatial points (sf)
+  #-----------------------------------------------
+  p_te                       <- te %>%
+    sf::st_as_sf(coords = c("SI_LONG", "SI_LATI")) %>%
+    sf::st_set_crs(4326)
+  
+  #-----------------------------------------------
+  # Add MPAs to file and save points inside polygons
+  #-----------------------------------------------
+  sub                        <- st_join(p_te, MPAs, join = st_intersects)
+  sub                        <- as.data.table(st_drop_geometry(sub[!is.na(sub$SITECODE),]))
+  saveRDS(sub, paste0(intPath, "MPA_", iYear, ".rds"))
+} # end iYear-loop
+rm(te, p_te, sub)
+
+#-----------------------------------------------
+# Obtain data for entire time period
+#-----------------------------------------------
+m_list                       <- list.files(path = intPath, pattern= "MPA", full.names = T)
+dat                          <- rbindlist(lapply(m_list, readRDS), fill=T)
+# Set NA's to 0 
+dat[is.na(dat)]              <-  0
+
+#-----------------------------------------------
+# Create table 2
+#-----------------------------------------------
+# Aggregate data and write to results folder
+table2                       <- dat[, .(Land_kg = sum(LE_KG_TOT),
+                                        Land_eu = sum(LE_EURO_TOT),
+                                        Eff_day = sum(LE_EFF_DAY, na.rm=T),
+                                        Eff_kwh = sum(LE_EFF_KWD, na.rm=T)),
+                                    by=.(LE_YEAR, LE_QUART, SITECODE, LE_MET)]
+
+# Save as csv-file
+fwrite(table2, paste0(resPath, "Table_2_", Country, ".csv"))
+
+#-----------------------------------------------
+# Create table 3a
+#-----------------------------------------------
+table3a                      <- data.table(SITECODE       = character(),
+                                           Spec_1_Biomass = character(),
+                                           Spec_2_Biomass = character(),
+                                           Spec_3_Biomass = character(),
+                                           Spec_4_Biomass = character(),
+                                           Spec_5_Biomass = character(),
+                                           Rest_Biomass   = numeric())
+
+for(iMPA in unique(dat$SITECODE)){
+  subdat                    <- dat[SITECODE == iMPA,,]
+  idx                       <- grep("LE_KG", names(subdat))
+  a                         <- sort(colSums(subdat[, ..idx,]), decreasing=TRUE)
+  dt                        <- data.table(SITECODE       = iMPA,
+                                          Spec_1_Biomass = substr(names(a)[2], 7, 9),
+                                          Spec_2_Biomass = substr(names(a)[3], 7, 9),
+                                          Spec_3_Biomass = substr(names(a)[4], 7, 9),
+                                          Spec_4_Biomass = substr(names(a)[5], 7, 9),
+                                          Spec_5_Biomass = substr(names(a)[6], 7, 9),
+                                          Rest_Biomass   = round((sum(a[7:length(a)]) / a[1]) *100, digits=1))
+  table3a                   <- rbind(table3a, dt)
+} # end iMPA-loop
+
+# Save as csv-file
+fwrite(table3a, paste0(resPath, "Table_3a_", Country, ".csv"))
+
+#-----------------------------------------------
+# Create table 3b
+#-----------------------------------------------
+table3b                      <- data.table(SITECODE     = character(),
+                                           Spec_1_Value = character(),
+                                           Spec_2_Value = character(),
+                                           Spec_3_Value = character(),
+                                           Spec_4_Value = character(),
+                                           Spec_5_Value = character(),
+                                           Rest_Value   = numeric())
+
+for(iMPA in unique(dat$SITECODE)){
+  subdat                    <- dat[SITECODE == iMPA,,]
+  idx                       <- grep("LE_EURO", names(subdat))
+  a                         <- sort(colSums(subdat[, ..idx,]), decreasing=TRUE)
+  dt                        <- data.table(SITECODE     = iMPA,
+                                          Spec_1_Value = substr(names(a)[2], 9, 11),
+                                          Spec_2_Value = substr(names(a)[3], 9, 11),
+                                          Spec_3_Value = substr(names(a)[4], 9, 11),
+                                          Spec_4_Value = substr(names(a)[5], 9, 11),
+                                          Spec_5_Value = substr(names(a)[6], 9, 11),
+                                          Rest_Value   = round((sum(a[7:length(a)]) / a[1]) *100, digits=1))
+  table3b                   <- rbind(table3b, dt)
+} # end iMPA-loop
+
+# Save as csv-file
+fwrite(table3b, paste0(resPath, "Table_3b_", Country, ".csv"))
+
+rm(m_list, dat, table2, table3a, table3b, subdat, idx, a, iMPA)
+#-----------------------------------------------
+
+
+
+################################################
+# Create Tables 4a and 4b
+################################################
+#-----------------------------------------------
+# Create table 4a
+#-----------------------------------------------
+# Obtain data for entire time period
+m_list                       <- list.files(path = intPath, pattern= "TACEFL", full.names = T)
+dat                          <- rbindlist(lapply(m_list, readRDS), fill=T)
+# Set NA's to 0 
+dat[is.na(dat)]              <-  0
+
+# Aggregate data and write to results folder
+table4a                      <- dat[, .(Land_kg = sum(LE_KG_TOT)),
+                                    by=.(LE_YEAR, LE_QUART, LE_RECT, LE_MET)]
+# Save as csv-file
+fwrite(table4a, paste0(resPath, "Table_4a_", Country, ".csv"))
+rm(m_list, dat, table4a)
+
+#-----------------------------------------------
+# Create table 4b
+#-----------------------------------------------
+# Obtain data for entire time period
+m_list                       <- list.files(path = intPath, pattern= "EFLA", full.names = T)
+dat                          <- rbindlist(lapply(m_list, readRDS), fill=T)
+# Change NA to 0 (only numeric columns, the other one doesnt work when there is NA's in timedate columns)
+for(j in seq_along(dat)){
+  set(dat3, i = which(is.na(dat3[[j]]) & is.numeric(dat3)), j = j, value = 0)
+} 
+
+# Aggregate data and write to results folder
+table4b                      <- dat[, .(Land_kg = sum(LE_KG_TOT)),
+                                    by=.(LE_YEAR, LE_QUART, LE_RECT, LE_MET)]
+# Save as csv-file
+fwrite(table4b, paste0(resPath, "Table_4b_", Country, ".csv"))
+rm(m_list, dat, table4b)
+#-----------------------------------------------
+
+
